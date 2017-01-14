8cfe7e3b6c3419e9b7811e0c2e2ba4f50bed1b16
Alexsandro Olivo <olivo.ale@hotmail.com>
2017-01-14 12:51:28 -0200

    Inclusão da biblioteca do gerenciamento WiFi
    SSID ESP8266: Agrotechlink
    Senha: agrotechlink

--- WeatherStationModel-0/WeatherStationModel-0.ino
+++ WeatherStationModel-0/WeatherStationModel-0.ino
@@ -1,17 +1,17 @@
-/*                    VERSÃO FINAL DO CODIGO                    
-                VERSÃO 1.0.0      DATA: 12/01/2017
-                   HORA-ULTIMO UPDATE: 1:29 a.m
-                COMPILADO NA VERSAO ARDUINO: ....
+/*                    VERSÃO FINAL DO CODIGO
+                VERSÃO 1.0.1      DATA: 12/01/2017
+                COMPILADO NA VERSAO ARDUINO: 1.8.1
                 __________________________________
 
                 VERSAO DO PROGRAMA ARDUINO: 1.8.1
-                PROGRAMA ATUALIZADO EM: 12/01/2017
+                PROGRAMA ATUALIZADO EM: 14/01/2017
+                HORA-ULTIMO UPDATE: 12:14 p.m
                 __________________________________
-                
+
                  PLACA WIFI ESP8266-07 AT THINKER
                  PROGRAMA: MINI ESTACAO CLIMATICA
                  CONTÉM SENSORES: BMP-180 E DHT22
-                __________________________________  
+                __________________________________
 
                 CONFIGURACAO DA PLACA PARA GRAVACAO
                 PLACA: GENERIC ESP8266 MODULE
@@ -24,7 +24,7 @@
                 RESET MOTHOD: ck
                 UPLOAD SPEED: 115200
                 PORTA: PORTA ESP CONECTADA AO COMPUTADOR
-                _________________________________              
+                _________________________________
 */
 /* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -*/
 // AGROTECHLINK.COM - ESP8266 - PROGRAM HEADER TEMPLATE - 2017 - JANUARY
@@ -32,7 +32,7 @@
 /* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -*/
 /*   CADA GPIO POSSUI UMA IDENTIFICACAO ESPECIFICA
      PORTAS UTILIZADAS NAS PLACAS DA MINI ESTACAO CLIMATICA
-     
+
      ATL3     >--> GPIO-16 + LED0
      ATL4     >--> GPIO-14 + BUZZER
      ATL5     >--> GPIO-12 + SENSOR DHT22 (TEMPERATURA-HUMIDADE)
@@ -51,18 +51,21 @@
      FTDI-RX  >--> ATL-TX
      FTDI-3V3 >--> ATL-3V3
      FTDI-0V  >--> ATL-0V
-     
+
      NUNCA ALIMENTAR ESTE MODULO DIRETAMENTE PELO GRAVADOR
-     OU USB DO COMPUTADOR!  
+     OU USB DO COMPUTADOR!
 */
 /* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -*/
 // LIVRARIAS EXTERNAS PARA FUNCIONAMENTO DOS SENSORES, CONEXÃO E DADOS
 /* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -*/
+#include     <FS.h>                // BIBLIOTECA WiFi DO ESP8266. DEVE SER SEMPRE A PRIMEIRA BIBLIOTECA MENCIONADA NO #INLUDE!!!!!!!
+#include     <ESP8266WiFi.h>       // BIBLIOTECA WiFi DO ESP8266
+#include     <DNSServer.h>         // BIBLIOTECA WiFi DO ESP8266
+#include     <ESP8266WebServer.h>  // BIBLIOTECA WiFi DO ESP8266
+#include     "WiFiManager.h"       // BIBLIOTECA WiFi-MANANGER DO ESP8266
 #include     <SFE_BMP180.h>        // SENSOR BMP-180 (PRESSAO) 
 #include     <Wire.h>              // NECESSÁRIO PARA COMUNICACAO I2C (PRESSAO)
-#include     "DHT.h"               // SENSOR DHT22 OU DHT11 (TEMPERATURA-HUMIDADE)
-#include     <ESP8266WiFi.h>       // BIBLIOTECA WiFi DO ESP8266
-#include     <WiFiClient.h>        // BIBLIOTECA WiFi DO ESP8266
+#include     "DHT.h"               // SENSOR DHT22 OU DHT11 (TEMPERATURA-HUMIDADE)   
 #include     <MySQL_Connection.h>  // CONEXAO COM BANCO DE DADOS
 #include     <MySQL_Cursor.h>      // CONEXAO COM BANCO DE DADOS
 /* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -*/
@@ -82,7 +85,7 @@
 #define      DHTTYPE    DHT22        // ESPECIFICACAO DO SENSOR UTILIZADO
 /*#define      DHTTYPE    DHT11*/    // DESMARCAR ESTE DEFINE CASO UTILIZAR O DHT11 E SUBLINHAR DHT22!!!
 #define      ALTITUDE   4.5          // ALTITUDE DA LOCALIZACAO DO ESP8266: JARAGUÁ DO SUL - SC (EM METROS - USAR PONTO AO INVES DA VIRGULA. Ex.:(23.3))
-DHT          dht (DHTPIN, DHTTYPE);  // ENDERECAMENTO DO SENSOR DHT22 
+DHT          dht (DHTPIN, DHTTYPE);  // ENDERECAMENTO DO SENSOR DHT22
 SFE_BMP180   pressao;                // DEFINICAO DO SENSOR BMP-180
 /* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -*/
 // DEFINICAO DAS VARIAVEIS GLOBAIS
@@ -90,36 +93,45 @@ SFE_BMP180   pressao;                // DEFINICAO DO SENSOR BMP-180
 double      baseline, P_bmp, T_bmp;  // VARIAVEIS PARA O SENSOR BMP-180
 float       T_dht, U_dht;            // VARIAVEIS PARA O SENSOR DHT22 OU DHT11 (CASO HABILITADO)
 unsigned    T = 0;                   // DELETE AFTER TESTS ARE DONE
+int         nCon, contNcon;          // VARIAVEIS PARA A BIBLIOTECA WiFi MANANGER (ASSEGURAR A CONEXAO COM A INTERNET EM CASO DE ERROS)
 /* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -*/
 /* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -*/
-// CONFIGURACOES WiFi / INTERNET E BANCO DE DADOS
+// CONFIGURACOES DE ACESSO AO BANCO DE DADOS
 /* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -*/
 IPAddress   server_addr (186, 202, 127, 122);   // IP DO MySQL SERVER - SITE AGROTECHLINK.COM
 char        user[] = "agrotech_u_intel";        // USUARIO DO BANCO DE DADOS
 char        password[] = "OlvAgrotechlink1357"; // SENHA DO USUARIO
-// TEMPORARIO CODIGO INTERNET. VAI SER SUBSTITUIDO PELA BIBLIOTECA WIFI.
- char        ssid[] = "GVT-A0F8"; // your SSID
- char        pass[] = "N1B9024257"; // your SSID Password
-/* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -*/
+
 WiFiClient client;
 MySQL_Connection conn((Client *)&client);
 /* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -*/
 /* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -*/
-// LED BLINK FUNCTION
+// MODO CONFIGURAÇÃO DO WiFi MANANGER
+/* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -*/
+void configModoCallback (WiFiManager *myWiFiManager) {
+  Serial.println("Iniciando modo de configuracao.");
+  Serial.println(WiFi.softAPIP());
+  Serial.println(myWiFiManager->getConfigPortalSSID());
+}
+/* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -*/
+/* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -*/
+// FAZER O LED PISCAR
 /* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -*/
 void LedATLblinks(unsigned M) {
   Serial.print("\nJUST LED0 IS BLINKING N TIMES: ");    Serial.println(M);
   for (short j = 0; j < M; j++) {
     Serial.print(j + 1);  Serial.print(" . ");
     digitalWrite(ATL3, HIGH);                             delay(100);
-    digitalWrite(ATL3, LOW);                              delay(2500);}
+    digitalWrite(ATL3, LOW);                              delay(2500);
+  }
   digitalWrite(ATL3, LOW);
   digitalWrite(LED_BUILTIN, LOW);                         delay(100);
   digitalWrite(LED_BUILTIN, HIGH);
   Serial.print("\n| - - - - - - BUILTIN LED BLINKS! - - - - - - - - |");
-Serial.println("\n| - - - - - - - - - - - - - - - - - - - - - - - - |");}
+  Serial.println("\n| - - - - - - - - - - - - - - - - - - - - - - - - |");
+}
 /* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -*/
-// PRESSAO E TEMPERATURA DO BMP180
+// PRESSAO E TEMPERATURA DO BMP180 - ESPECIAL
 /* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -*/
 float getPressure() {
   char status;
@@ -133,13 +145,18 @@ float getPressure() {
         delay(status);
         status = pressao.getPressure(P_bmp, T_bmp);
         if (status != 0) {
-          return (P_bmp, T_bmp);}
-        else Serial.print("\n>--> Erro ajustando - P - ESPECIAL!");}
-      else Serial.print("\n>--> Erro na leitura - P - ESPECIAL!");}
-    else Serial.print("\n>--> Erro ajustando temperatura BMP - ESPECIAL!");}
-  else Serial.print("\n>--> Erro na leitura da temperatura BMP - ESPECIAL!");}
+          return (P_bmp, T_bmp);
+        }
+        else Serial.print("\n>--> Erro ajustando - P - ESPECIAL!");
+      }
+      else Serial.print("\n>--> Erro na leitura - P - ESPECIAL!");
+    }
+    else Serial.print("\n>--> Erro ajustando temperatura BMP - ESPECIAL!");
+  }
+  else Serial.print("\n>--> Erro na leitura da temperatura BMP - ESPECIAL!");
+}
 /* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -*/
-// GET BMPP180 VALUES FUNCTION
+// GET DADOS DE TEMPERATURA E PRESSAO DO BMP-180
 /* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -*/
 void GetATLbmpPT() { // SENSOR BMP180
   getPressure();
@@ -148,13 +165,17 @@ void GetATLbmpPT() { // SENSOR BMP180
     getPressure();
     if (isnan(P_bmp)) {
       Serial.println("\n>--> Falha na leitura - p - do sensor BMP180!");
-      return;}}
+      return;
+    }
+  }
   if (isnan(T_bmp)) {
     delay(500);
     getPressure();
     if (isnan(T_bmp)) {
       Serial.println("\n>--> Falha na leitura - T - do sensor BMP180!");
-      return;}}
+      return;
+    }
+  }
   Serial.print("\n| Pressao atmosferica......BMP: ");
   Serial.print(P_bmp, 2);
   Serial.print(" hPa");
@@ -166,19 +187,20 @@ void GetATLbmpPT() { // SENSOR BMP180
   Serial.print("\n| - - - - - - - - - - - - - - - - - - - - - - - - |");
 }
 /* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -*/
-// GET DHT TEMPERATURE AND HUMIDIYT VALUES FUNCTION
+// GET DADOS DE TEMPERATURA E HUMIDADE DO DHT22 OU DHT11
 /* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -*/
-void GetATLdhtTU() { // SENSOR AM2302
+void GetATLdhtTU() {
   U_dht = dht.readHumidity();
   T_dht = dht.readTemperature();
   if (isnan(U_dht) || isnan(T_dht)) {
-    delay(2000);
-// WHEN DHT22 IS USED SET THIS TO 2000, OTHERWISE SET TO 5000
+    delay(2000); // WHEN DHT22 IS USED SET THIS TO 2000, OTHERWISE SET TO 5000
     U_dht = dht.readHumidity();
     T_dht = dht.readTemperature();
     if (isnan(U_dht) || isnan(T_dht)) {
       Serial.println("\n>--> Falha na leitura do Sensor DHT!");
-      return;}}
+      return;
+    }
+  }
   Serial.print("\n| Temperatura ambiente.....DHT: ");
   Serial.print(T_dht);
   Serial.print(" *C");
@@ -187,73 +209,87 @@ void GetATLdhtTU() { // SENSOR AM2302
   Serial.print(U_dht);
   Serial.print(" %UR");
   Serial.println(" - - - - |");
-  Serial.print("\n| - - - - - - - - - - - - - - - - - - - - - - - - |");}
+  Serial.print("\n| - - - - - - - - - - - - - - - - - - - - - - - - |");
+}
 /* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -*/
-void setup() {                         // COMMENT - UNCOMMENT AS NECESSARY
+void setup() {
   Serial.begin(115200);
-  pinMode(LED_BUILTIN, OUTPUT);       // Initialize the LED_BUILTIN - output
-  digitalWrite(LED_BUILTIN, HIGH);      // turn LED OFF - acive low on ESP07
+  pinMode(LED_BUILTIN, OUTPUT);         // INICIALIZA O LED_BUILTIN NATIVO DO ESP8266
+  digitalWrite(LED_BUILTIN, HIGH);      // DESLIGA O LED_BUILTIN NATIVO DO ESP8266
   pinMode(ATL3, OUTPUT);      digitalWrite(ATL3, LOW);  // GPIO-16 + LED0
   pinMode(ATL4, OUTPUT);      digitalWrite(ATL4, LOW);  // GPIO-14 + BUZZER
   /*----------------------------------------------------------------------*/
+  WiFiManager wifiManager;
+  wifiManager.setAPCallback(configModoCallback);
+  if (!wifiManager.autoConnect("Agrotechlink", "agrotechlink")) {
+    Serial.println("Falha na conexao. Reiniciando");
+    contNcon = (nCon + 1);
+    delay(5000);
+    ESP.reset();
+    delay(2000);
+    if (contNcon == 5) {
+      wifiManager.resetSettings();      // CASO OCORRA FALHAS EXEPCIONAIS NA CONEXAO O ESP8266 RESETA E INICIA NOVAMENTE
+      delay(5000);
+      ESP.reset();
+      delay(2000);
+    }
+  }
+  Serial.println("Internet conectada. Wi-fi client em rede :)");
+  delay(500);
+  /*----------------------------------------------------------------------*/
   Serial.println("| - - - - - - - - - - - - - - - - - - - - - - - - |");
   Serial.println("| AGROTECHLINK - RADIO BOARD TEST - ESP8266 - - - |");
-  Serial.println("| Iniciao da verificacao sensores:  BMP180 & DHT22|");
+  Serial.println("| Iniciao da verificacao sensores:  BMP-180 & DHT22|");
   Serial.println("| Informacoes do ESP8266 - - - - - - - - - - - -  |");
   Serial.print("| MAC NUMBER: " + WiFi.macAddress());
   Serial.println(" - - - - - - - - - |");
-//  Serial.print("| Chip ID: %08X\n" + ESP.getFlashChipId());
-//  Serial.println(" - - - - - - - - - |");
+  Serial.print("| IP LOCAL: " + WiFi.localIP());
+  Serial.println(" - - - - - - - - - |");
   Serial.println("| AGROTECHLINK - TODOS OS DIREITOS SAO RESERVADOS |");
   Serial.println("| - - - - - - - - - - - - - - - - - - - - - - - - |");
-/* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -*/
-  WiFi.begin(ssid, pass);
-  // Wait for connection
-  while ( WiFi.status() != WL_CONNECTED ) {
-    delay ( 500 );
-    Serial.print ( "." );}
-  Serial.println ( "" );
-  Serial.println("| - - - - - - - - - - - - - - - - - - - - - - - - |");
-  Serial.print("| Connected to:  ");
-  Serial.println (ssid);
-  Serial.print ("| IP address: ");
-  Serial.println ( WiFi.localIP() );
-// End WiFi section
-  Serial.println("| DB - Connecting...   \n");
+  /* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -*/
+  Serial.println("| Conectando ao banco de dados da Agrotechlink... |\n");
   while (conn.connect(server_addr, 3306, user, password) != true) {
     delay(500);
-    Serial.print ( "." );}
+    Serial.print ( "." );
+  }
   delay(500);
-  Serial.println("| - - - - - - - - - - - - - - - - - - - - - - - - |");  
-  dht.begin();
-  pressao.begin();
-  getPressure();}
+  
+  Serial.println("| - - - - - - - - - - - - - - - - - - - - - - - - |");
+  Serial.println("| Conexao com o banco de dados bem sucedida! (:   |\n");
+  /* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -*/
+  dht.begin();                      // INICIANDO SENSOR DE TEMPERATURA DHT22
+  pressao.begin();                  // INICIANDO SENSOR DE PRESSAO BMP-180
+  getPressure();                    // SETANDO CONFIGURAÇÕES ESPECIAIS DO BMP-180
+}
 /* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -*/
-// TEST RADIO MODULE BOARD MAIN FUNCTION - INCLUDED: LED0&1+BUZZER
+// FIM DO SETUP E CONFIGURACOES. INICIO DO LOOP.
 /* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -*/
 void loop() {
-  LedATLblinks(T);
-  GetATLbmpPT();
-  GetATLdhtTU();
-  T++;
+  LedATLblinks(T);              // LED
+  GetATLbmpPT();                // BMP-180
+  GetATLdhtTU();                // DHT22
+  T++;                          // VERIFICACAO DO LED
   if (T == 10) T = 0;
-/* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -*/
-  char ST_dht[6],SU_dht[6], ST_bmp[6], SP_bmp[8], query[82];
-  dtostrf(T_dht,2, 2, ST_dht);
-  dtostrf(U_dht,2, 2, SU_dht);
-  dtostrf(T_bmp,2, 2, ST_bmp);
-  dtostrf(P_bmp,4, 2, SP_bmp);
-/* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -*/
+  /* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -*/
+  char ST_dht[6], SU_dht[6], ST_bmp[6], SP_bmp[8], query[82];  // CONVERTENDO DADOS DOS SENSORES PARA STRING
+  dtostrf(T_dht, 2, 2, ST_dht);
+  dtostrf(U_dht, 2, 2, SU_dht);
+  dtostrf(T_bmp, 2, 2, ST_bmp);
+  dtostrf(P_bmp, 4, 2, SP_bmp);
+  /* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -*/
   char INSERT_SQL[] = "INSERT INTO agrotech_intel.teste VALUES (NULL, %s, %s, %s, %s);";
-  sprintf(query, INSERT_SQL, ST_dht, SU_dht, ST_bmp, SP_bmp);
+  sprintf(query, INSERT_SQL, ST_dht, SU_dht, ST_bmp, SP_bmp);  // CONCATENANDO A STRING INSERT_SQL PARA GRAVACAO NO BANCO DE DADOS
   MySQL_Cursor *cur_mem = new MySQL_Cursor(&conn);
-  Serial.println("\n| Executando querry");
+  Serial.println("\n| Executando querry no banco de dados");
   cur_mem->execute(query);
   Serial.println("| Querry INSERT:  ");
   Serial.println(query);
   Serial.println("| Limpando dados de requisicao da memoria.");
   delete cur_mem;
-  delay(1000);}
+  Serial.println("| Execucao de querry bem sucedida!");
+  delay(1000);
+}
 /* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -*/
 // MAIN FUNCTION END - FINAL
 /* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -*/
