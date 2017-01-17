ccf0c572b5465ecd3e430d4c647de1c552dbee71
Alexsandro Olivo <olivo.ale@hotmail.com>
2017-01-17 01:29:06 -0200

    Adicionado data e hora e ajustes visual no pisca do LED
    Incluido a biblioteca TIME do Arduino;
    Conexão NTP com servidor de hora; e
    Ajuste visual do LED para piscar quando setup = ok e gravação no BD =
    ok.
    *Corrigir alguns pontos entre a definição do wifi e NTP no Setup!!
    (farei logo logo ((: )

--- WeatherStationModel-0/WeatherStationModel-0.ino
+++ WeatherStationModel-0/WeatherStationModel-0.ino
@@ -4,8 +4,8 @@
                 __________________________________
 
                 VERSAO DO PROGRAMA ARDUINO: 1.8.1
-                PROGRAMA ATUALIZADO EM: 16/01/2017
-                HORA-ULTIMO UPDATE: 11:28 a.m.
+                PROGRAMA ATUALIZADO EM: 17/01/2017
+                HORA-ULTIMO UPDATE: 00:20 a.m.
                 __________________________________
 
                  PLACA WIFI ESP8266-07 AT THINKER
@@ -63,6 +63,8 @@
 #include     <DNSServer.h>           // BIBLIOTECA WiFi DO ESP8266
 #include     <ESP8266WebServer.h>    // BIBLIOTECA WiFi DO ESP8266
 #include     "WiFiManager.h"         // BIBLIOTECA WiFi-MANANGER DO ESP8266
+#include     <TimeLib.h>             // BIBLIOTECA DE DATA E HORA DO ESP8266
+#include     <WiFiUdp.h>             // BIBLIOTECA DE DATA E HORA DO ESP8266
 #include     <SFE_BMP180.h>          // SENSOR BMP-180 (PRESSAO) 
 #include     <Wire.h>                // NECESSÁRIO PARA COMUNICACAO I2C (PRESSAO)
 #include     "DHT.h"                 // SENSOR DHT22 OU DHT11 (TEMPERATURA-HUMIDADE)   
@@ -85,16 +87,19 @@
 #define      DHTTYPE    DHT22        // ESPECIFICACAO DO SENSOR UTILIZADO
 /*#define      DHTTYPE    DHT11*/    // DESMARCAR ESTE DEFINE CASO UTILIZAR O DHT11 E SUBLINHAR DHT22!!!
 #define      ALTITUDE   4.5          // ALTITUDE DA LOCALIZACAO DO ESP8266: JARAGUÁ DO SUL - SC (EM METROS - USAR PONTO AO INVES DA VIRGULA. Ex.:(23.3))
+WiFiUDP      Udp;                    // DEFINICAO DA BIBLIOTECA UDP DATA E HORA
 DHT          dht (DHTPIN, DHTTYPE);  // ENDERECAMENTO DO SENSOR DHT22
 SFE_BMP180   pressao;                // DEFINICAO DO SENSOR BMP-180
 /* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -*/
 // DEFINICAO DAS VARIAVEIS GLOBAIS
 /* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -*/
-double      baseline, P_bmp, T_bmp;  // VARIAVEIS PARA O SENSOR BMP-180
-float       T_dht, U_dht;            // VARIAVEIS PARA O SENSOR DHT22 OU DHT11 (CASO HABILITADO)
-unsigned    T = 0;                   // DELETE AFTER TESTS ARE DONE
-int         nCon, contNcon, fMysql;  // VARIAVEIS PARA A BIBLIOTECA WiFi MANANGER E MySQL (ASSEGURAR A CONEXAO COM A INTERNET E MySQL EM CASO DE ERROS)
-/* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -*/
+static const char   ntpServerName[] = "a.st1.ntp.br";    // DEFINICAO DO SERVIDOR NTP DE HORA BRASILEIRO
+const int           timeZone = -2, NTP_PACKET_SIZE = 48; // DEFINIDO COMO -2 DEVIDO AO HORÁRIO DE VERÃO. DEPOIS DO DIA 19/02 MUDAR PARA -3!
+unsigned int        localPort = 8888;                    // PORTAL LOCAL PARA OS PACOTES UDP
+byte                packetBuffer[NTP_PACKET_SIZE];       // BUFFER PARA OS PACOTES DE DATA E HORA
+double              baseline, P_bmp, T_bmp;              // VARIAVEIS PARA O SENSOR BMP-180
+float               T_dht, U_dht;                        // VARIAVEIS PARA O SENSOR DHT22 OU DHT11
+int                 nCon, contNcon, fMysql;              // VARIAVEIS PARA WiFi MANANGER E MySQL (CONEXAO COM A INTERNET E MySQL EM CASO DE ERROS)
 /* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -*/
 // CONFIGURACOES DE ACESSO AO BANCO DE DADOS
 /* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -*/
@@ -104,6 +109,75 @@ char        password[] = "OlvAgrotechlink1357"; // SENHA DO USUARIO
 
 WiFiClient client;
 MySQL_Connection conn((Client *)&client);
+/* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -*/
+// FORMATACAO DA BIBLIOTECA DE DATA E HORA
+/* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -*/
+time_t      getNtpTime();
+void        relogioDisplay();
+void        printDigits(int digits);
+void        sendNTPpacket(IPAddress &address);
+/* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -*/
+// BIBLIOTECAS DE DATA E HORA
+/* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -*/
+time_t prevDisplay = 0;
+time_t getNtpTime()
+{
+  IPAddress ntpServerIP;
+  while (Udp.parsePacket() > 0) ;
+  WiFi.hostByName(ntpServerName, ntpServerIP);
+  sendNTPpacket(ntpServerIP);
+  uint32_t beginWait = millis();
+  while (millis() - beginWait < 1500) {
+    int size = Udp.parsePacket();
+    if (size >= NTP_PACKET_SIZE) {
+      Udp.read(packetBuffer, NTP_PACKET_SIZE);
+      unsigned long secsSince1900;
+      secsSince1900 =  (unsigned long)packetBuffer[40] << 24;
+      secsSince1900 |= (unsigned long)packetBuffer[41] << 16;
+      secsSince1900 |= (unsigned long)packetBuffer[42] << 8;
+      secsSince1900 |= (unsigned long)packetBuffer[43];
+      return secsSince1900 - 2208988800UL + timeZone * SECS_PER_HOUR;
+    }
+  }
+  return 0;
+}
+void relogioDisplay()
+{
+  Serial.println("| DATA E HORA ATUAL - - - - - - - - - - - - - - - |");
+  Serial.println("| HORA: - - - - - - - - - - - - - - - - - - - - - |");
+  Serial.print(hour());
+  printDigits(minute());
+  printDigits(second());
+  Serial.println("| DATA: - - - - - - - - - - - - - - - - - - - - - |");
+  Serial.print(day());
+  Serial.print("-");
+  Serial.print(month());
+  Serial.print("-");
+  Serial.print(year());
+  Serial.println();
+}
+void printDigits(int digits)
+{
+  Serial.print(":");
+  if (digits < 10)
+    Serial.print('0');
+  Serial.print(digits);
+}
+void sendNTPpacket(IPAddress &address)
+{
+  memset(packetBuffer, 0, NTP_PACKET_SIZE);
+  packetBuffer[0] = 0b11100011;
+  packetBuffer[1] = 0;
+  packetBuffer[2] = 6;
+  packetBuffer[3] = 0xEC;
+  packetBuffer[12] = 49;
+  packetBuffer[13] = 0x4E;
+  packetBuffer[14] = 49;
+  packetBuffer[15] = 52;
+  Udp.beginPacket(address, 123);
+  Udp.write(packetBuffer, NTP_PACKET_SIZE);
+  Udp.endPacket();
+}
 /* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -*/
 // MODO CONFIGURAÇÃO DO WiFi MANANGER
 /* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -*/
@@ -116,17 +190,13 @@ void configModoCallback (WiFiManager *myWiFiManager) {
 // FAZER O LED PISCAR
 /* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -*/
 void LedATLblinks(unsigned M) {
-  Serial.print("\nJUST LED0 IS BLINKING N TIMES: ");    Serial.println(M);
   for (short j = 0; j < M; j++) {
-    Serial.print(j + 1);  Serial.print(" . ");
-    digitalWrite(ATL3, HIGH);                             delay(100);
-    digitalWrite(ATL3, LOW);                              delay(2500);
+    digitalWrite(ATL3, HIGH);                             delay(250);
+    digitalWrite(ATL3, LOW);                              delay(250);
   }
   digitalWrite(ATL3, LOW);
-  digitalWrite(LED_BUILTIN, LOW);                         delay(100);
+  digitalWrite(LED_BUILTIN, LOW);                         delay(250);
   digitalWrite(LED_BUILTIN, HIGH);
-  Serial.print("\n| - - - - - - BUILTIN LED BLINKS! - - - - - - - - |");
-  Serial.println("\n| - - - - - - - - - - - - - - - - - - - - - - - - |");
 }
 /* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -*/
 // PRESSAO E TEMPERATURA DO BMP180 - ESPECIAL
@@ -191,8 +261,8 @@ void GetATLdhtTU() {
   U_dht = dht.readHumidity();
   T_dht = dht.readTemperature();
   if (isnan(U_dht) || isnan(T_dht)) {
-    delay(2000); 
-// WHEN DHT22 IS USED SET THIS TO 2000, OTHERWISE SET TO 5000
+    delay(2000);
+    // WHEN DHT22 IS USED SET THIS TO 2000, OTHERWISE SET TO 5000
     U_dht = dht.readHumidity();
     T_dht = dht.readTemperature();
     if (isnan(U_dht) || isnan(T_dht)) {
@@ -218,9 +288,9 @@ void setup() {
   pinMode(LED_BUILTIN, OUTPUT);         // INICIALIZA O LED_BUILTIN NATIVO DO ESP8266
   digitalWrite(LED_BUILTIN, HIGH);      // DESLIGA O LED_BUILTIN NATIVO DO ESP8266
   pinMode(ATL3, OUTPUT);      digitalWrite(ATL3, LOW);  // GPIO-16 + LED0
-//  pinMode(ATL4, OUTPUT);      digitalWrite(ATL4, LOW);  
-//  ATL4 --> GPIO-14 + BUZZER - NAO USADO NA ESTACAO CLIMATICA!
-/* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -*/
+  //  pinMode(ATL4, OUTPUT);      digitalWrite(ATL4, LOW);
+  //  ATL4 --> GPIO-14 + BUZZER - NAO USADO NA ESTACAO CLIMATICA!
+  /* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -*/
   WiFiManager wifiManager;
   wifiManager.setAPCallback(configModoCallback);
   if (!wifiManager.autoConnect("Agrotechlink", "agrotechlink")) {
@@ -230,8 +300,8 @@ void setup() {
     ESP.restart();
     delay(2000);
     if (contNcon == 3) {
-      wifiManager.resetSettings();      
-// CASO OCORRA FALHAS EXEPCIONAIS NA CONEXAO O ESP8266 RESETA E INICIA NOVAMENTE
+      wifiManager.resetSettings();
+      // CASO OCORRA FALHAS EXEPCIONAIS NA CONEXAO O ESP8266 RESETA E INICIA NOVAMENTE
       delay(5000);
       ESP.reset();
       delay(2000);
@@ -239,7 +309,7 @@ void setup() {
   }
   Serial.println("| Internet conectada. Wi-fi client em rede :)");
   delay(500);
-/* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -*/
+  /* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -*/
   Serial.println("| - - - - - - - - - - - - - - - - - - - - - - - - |");
   Serial.println("| AGROTECHLINK - RADIO BOARD TEST - ESP8266 - - - |");
   Serial.println("| Inicio verificacao de sensores: BMP-180 & DHT22 |");
@@ -248,9 +318,16 @@ void setup() {
   Serial.println(" - - - - - - - - - |");
   Serial.print("| IP LOCAL: " + WiFi.localIP());
   Serial.println(" - - - - - - - - - |");
+  Serial.println("| INICIANDO UDP - - - - - - - - - - - - - - - - - |");
+  Udp.begin     (localPort);
+  Serial.println("PORTA LOCAL UDP: - - - - - - - - - - - - - - - - |");
+  Serial.print(Udp.localPort()); Serial.println(" - - - - - - - - |");
+  Serial.println("| SINCRONIZANDO COM SERVIDOR NTP / TEMPO - - - - |");
+  setSyncProvider(getNtpTime);
+  setSyncInterval(300);
   Serial.println("| AGROTECHLINK - TODOS OS DIREITOS SAO RESERVADOS |");
   Serial.println("| - - - - - - - - - - - - - - - - - - - - - - - - |");
-/* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -*/
+  /* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -*/
   Serial.println("| Conectando ao banco de dados da Agrotechlink... |\n");
   while (conn.connect(server_addr, 3306, user, password) != true) {
     delay(500);
@@ -260,7 +337,9 @@ void setup() {
 
   Serial.println("| - - - - - - - - - - - - - - - - - - - - - - - - |");
   Serial.println("| Conexao com o banco de dados bem sucedida! (:   |\n");
-/* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -*/
+
+  LedATLblinks(3);           // LED. 3 VEZES = PRIMEIRA CONEXAO COM BD OK!
+  /* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -*/
   dht.begin();               // INICIANDO SENSOR DE TEMPERATURA DHT22
   pressao.begin();           // INICIANDO SENSOR DE PRESSAO BMP-180
   getPressure();             // SETANDO CONFIGURACOES ESPECIAIS DO BMP-180
@@ -269,23 +348,20 @@ void setup() {
 // FIM DO SETUP E CONFIGURACOES. INICIO DO LOOP.
 /* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -*/
 void loop() {
-  LedATLblinks(T);           // LED
   GetATLbmpPT();             // BMP-180
   GetATLdhtTU();             // DHT22
-  T++;                       // VERIFICACAO DO LED
-  if (T == 10) T = 0;
-/* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -*/
+  /* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -*/
   if (conn.connected()) {
-    char ST_dht[6], SU_dht[6], ST_bmp[6], SP_bmp[8], query[82];  
-// CONVERTENDO DADOS DOS SENSORES PARA STRING
+    char ST_dht[6], SU_dht[6], ST_bmp[6], SP_bmp[8], query[82];
+    // CONVERTENDO DADOS DOS SENSORES PARA STRING
     dtostrf(T_dht, 2, 2, ST_dht);
     dtostrf(U_dht, 2, 2, SU_dht);
     dtostrf(T_bmp, 2, 2, ST_bmp);
     dtostrf(P_bmp, 4, 2, SP_bmp);
-/* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -*/
+    /* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -*/
     char INSERT_SQL[] = "INSERT INTO agrotech_intel.teste VALUES (NULL, %s, %s, %s, %s);";
-    sprintf(query, INSERT_SQL, ST_dht, SU_dht, ST_bmp, SP_bmp);  
-// CONCATENANDO A STRING INSERT_SQL PARA GRAVACAO NO BANCO DE DADOS
+    sprintf(query, INSERT_SQL, ST_dht, SU_dht, ST_bmp, SP_bmp);
+    // CONCATENANDO A STRING INSERT_SQL PARA GRAVACAO NO BANCO DE DADOS
     MySQL_Cursor *cur_mem = new MySQL_Cursor(&conn);
     delay(500);
     Serial.println("\n| Executando querry no banco de dados - - - - - - |");
@@ -298,6 +374,14 @@ void loop() {
     delay(500);
     Serial.println("| Execucao de querry bem sucedida! - - - - - - -  |");
     fMysql = 0;
+
+    if (timeStatus() != timeNotSet) {
+      if (now() != prevDisplay) {
+        prevDisplay = now();
+        relogioDisplay();
+      }
+    }
+    LedATLblinks(2);           // LED. 2 VEZES = DADOS INSERIDOS NO BD!
 
   } else {
     conn.close();
