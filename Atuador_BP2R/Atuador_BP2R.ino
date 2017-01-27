87b631bd775419afec64fda428894c8f513eb8fb
Alexsandro Olivo <olivo.ale@hotmail.com>
2017-01-27 20:38:12 -0200

    Código do atuador versão COMERCIAL PRONTO
    Retirado o debug serial
    Feito algumas melhorias com o mysql

--- Atuador_BP2R/Atuador_BP2R.ino
+++ Atuador_BP2R/Atuador_BP2R.ino
@@ -66,8 +66,8 @@
 /* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -*/
 // DEFINICAO DAS VARIAVEIS GLOBAIS
 /* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -*/
-//static const char   CPF[] = "09084678931";          // CPF DO USUARIO. APENAS NUMEROS!!!!
-static const char   CPF[] = "01234567890";               // CPF DO USUARIO. APENAS NUMEROS!!!!
+static const char   CPF[] = "09084678931";          // CPF DO USUARIO. APENAS NUMEROS!!!!
+//static const char   CPF[] = "01234567890";               // CPF DO USUARIO. APENAS NUMEROS!!!!
 // ID DO PATO DONALD PARA TESTES...
 String              macAdress;
 char                query[200], S_macAdress[30];    // QUERY E MAC PARA BANCO DE DADOS
@@ -82,10 +82,16 @@ char        password[] = "OlvAgrotechlink1357"; // SENHA DO USUARIO
 WiFiClient client;
 MySQL_Connection conn((Client *)&client);
 /* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -*/
+// CONFIGURACAO WiFi
+/* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -*/
+void configModeCallback (WiFiManager *myWiFiManager) {
+  Serial.println(WiFi.softAPIP());
+  Serial.println(myWiFiManager->getConfigPortalSSID());
+}
+/* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -*/
 // FAZER O LED PISCAR
 /* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -*/
 void LedATLblinks(unsigned M) {
-  Serial.print("\nJUST LED0 IS BLINKING N TIMES: ");    Serial.println(M);
   for (short j = 0; j < M; j++) {
     Serial.print(j + 1);  Serial.print(" . ");
     digitalWrite(ATL3, HIGH);                     delay(300);
@@ -102,18 +108,12 @@ void atuaESP_BP2R(int comando, uint8_t PIN) {
   switch (comando) {
     case 1:
       digitalWrite(PIN, HIGH);
-      Serial.println("Ligado");
-      digitalWrite(LED_BUILTIN, LOW);
       break;
     case 0:
       digitalWrite(PIN, LOW);
-      Serial.println("Desligado");
-      digitalWrite(LED_BUILTIN, HIGH);
       break;
     default:
       digitalWrite(PIN, LOW);
-      Serial.println("Desligado. Modo DEFAULT.");
-      digitalWrite(LED_BUILTIN, HIGH);
       break;
   }
 }
@@ -121,7 +121,6 @@ void atuaESP_BP2R(int comando, uint8_t PIN) {
 // INICIO DO MODO SETUP DO ATUADOR ESP8266 BP-2R
 /* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -*/
 void setup() {
-  Serial.begin(115200);
   pinMode(LED_BUILTIN, OUTPUT);         // INICIALIZA O LED_BUILTIN NATIVO DO ESP8266
   digitalWrite(LED_BUILTIN, HIGH);      // DESLIGA O LED_BUILTIN NATIVO DO ESP8266
   pinMode(ATL3, OUTPUT);      digitalWrite(ATL3, LOW);  // GPIO-16 + LED0
@@ -129,7 +128,7 @@ void setup() {
   pinMode(ATL8, OUTPUT);      digitalWrite(ATL8, LOW);  // GPIO-04 + RELE 2 / B
   /* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -*/
   WiFiManager wifiManager;
-  wifiManager.setDebugOutput(false);
+  wifiManager.setAPCallback(configModeCallback);
   wifiManager.autoConnect("Agrotechlink", "agrotechlink");
   delay(500);
   /* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -*/
@@ -141,32 +140,12 @@ void setup() {
   macAdress = WiFi.macAddress();
   macAdress.toCharArray(S_macAdress, 30);
   /* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -*/
-  row_values *row = NULL;
-  long head_count = 0;
-
-  char SELECT_SQL[] = "SELECT id_controlador FROM agrotech_intel.controlador_BP2R WHERE mac_number = '%s'";
-  sprintf(query, SELECT_SQL, S_macAdress);
+  char INSERT_SQL[] = "INSERT INTO agrotech_intel.cont_BP2R SET cpf_usuario='%s', mac='%s'";
+  sprintf(query, INSERT_SQL, CPF, S_macAdress);
   MySQL_Cursor *cur_mem = new MySQL_Cursor(&conn);
   cur_mem->execute(query);
-  column_names *columns = cur_mem->get_columns();
-  Serial.println("Passou aqui 1");
-  do {
-    row = cur_mem->get_next_row();
-    if (row != NULL) {
-      head_count = atol(row->values[0]);
-    }
-  } while (row != NULL);
   delete cur_mem;
-  Serial.println(head_count);
-  /* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -*/
-  if (head_count == 0 || NULL) {
-    char  SRele1[] = "0", SRele2[] = "0", SAtivo[] = "1";
-    char INSERT_SQL[] = "INSERT INTO agrotech_intel.controlador_BP2R SET cpf_usuario ='%s', mac_number ='%s', rele_A ='%s', rele_B ='%s', ativo='%s';";
-    sprintf(query, INSERT_SQL, CPF, S_macAdress, SRele1, SRele2, SAtivo);
-    MySQL_Cursor *cur_mem = new MySQL_Cursor(&conn);
-    cur_mem->execute(query);
-    delete cur_mem;
-  }
+
   LedATLblinks(3);           // LED. 3 VEZES = PRIMEIRA CONEXAO COM BD OK!
 }
 /* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -*/
@@ -180,37 +159,36 @@ void loop() {
   /* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -*/
   if (conn.connected()) {                                       // VERIFICA A CONEXAO COM O DB DO MySQL
     macAdress.toCharArray(S_macAdress, 30);                     // CONVERTE O MAC DE STRING PARA CHAR
-    char SELECT_RELE_A_SQL[] = "SELECT rele_1 FROM agrotech_intel.teste_atuador WHERE mac_number = '%s';";
+    char SELECT_RELE_A_SQL[] = "SELECT r_A FROM agrotech_intel.cont_BP2R WHERE mac='%s'";
     sprintf(query, SELECT_RELE_A_SQL, S_macAdress);             // CONCATENANDO A STRING SELECT_SQL PARA BUSCA NO BANCO DE DADOS
 
     MySQL_Cursor *cur_mem_1 = new MySQL_Cursor(&conn);
     cur_mem_1 -> execute(query);
-    column_names *columns_1 = cur_mem_1 -> get_columns();        // PESQUISANDO POR COLUNAS. IMPORTANTE DEVIDO A MEMORIA DO ESP8266 SER POUCA
-    
-    do {                                                         // LE O ROW SELECIONADO
+    column_names *columns_1 = cur_mem_1 -> get_columns();       // PESQUISANDO POR COLUNAS. IMPORTANTE DEVIDO A MEMORIA DO ESP8266 SER POUCA
+
+    do {                                                        // LE O ROW SELECIONADO
       row_1 = cur_mem_1 -> get_next_row();
       if (row_1 != NULL) {
         head_count_1 = atol(row_1 -> values[0]);
       }
     } while (row_1 != NULL);
-    
+
     delete cur_mem_1;                                             // LIMPANDO A MEMORIA DO ESP8266
-    delay(500);
     /* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -*/
-    char SELECT_RELE_B_SQL[] = "SELECT rele_2 FROM agrotech_intel.teste_atuador WHERE mac_number = '%s';";
+    char SELECT_RELE_B_SQL[] = "SELECT r_B FROM agrotech_intel.cont_BP2R WHERE mac='%s'";
     sprintf(query, SELECT_RELE_B_SQL, S_macAdress);              // CONCATENANDO A STRING SELECT_SQL PARA BUSCA NO BANCO DE DADOS
 
     MySQL_Cursor *cur_mem_2 = new MySQL_Cursor(&conn);
     cur_mem_2 -> execute(query);
     column_names *columns_2 = cur_mem_2 -> get_columns();        // PESQUISANDO POR COLUNAS. IMMPORTANTE DEVIDO A MEMORIA DO ESP8266 SER POUCA
-    
+
     do {                                                         // LE O ROW SELECIONADO
       row_2 = cur_mem_2 -> get_next_row();
       if (row_2 != NULL) {
         head_count_2 = atol(row_2 -> values[0]);
       }
     } while (row_2 != NULL);
-    
+
     delete cur_mem_2;                                            // LIMPANDO A MEMORIA DO ESP8266
     Serial.println(head_count_1);                                // RESUTLADO DO DB DO RELE 1
     Serial.println(head_count_2);                                // RESUTLADO DO DB DO RELE 2
@@ -223,7 +201,7 @@ void loop() {
     if (conn.connect(server_addr, 3306, user, password)) {
       delay(500);
       fMysql++;
-      if (fMysql == 5) {
+      if (fMysql >= 7) {
         delay(5000);
         ESP.restart();
         delay(3000);
